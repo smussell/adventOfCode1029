17939c2712452b6fb0c27183cf9972a43e676125
Sam Mussell <smussell@gmail.com>
2019-12-04 18:04:03 -0800

    Update day2b.clj

--- day2b.clj
+++ day2b.clj
@@ -1,11 +1,12 @@
-(defn op [oper] 
-  (fn [p1 p2 out list]
-    (assoc list out (oper (get list p1) (get list p2)))))
+
+
+(defn op [oper p1 p2 out list]
+    (assoc list out (oper (get list p1) (get list p2))))
 
 (defn ops [n] 
   (get {
-    1 (op +)
-    2 (op *)
+    1 (partial op +)
+    2 (partial op *)
   } n))
 
 (defn exec [input]
